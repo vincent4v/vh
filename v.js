6eb22eb2f38c81020ce3a13107bcfee27840fa46
P338jf3 <41086933+vincent4v@users.noreply.github.com>
2019-04-17 20:34:44 -0500

    Update v.js

--- v.js
+++ v.js
@@ -3,3 +3,9 @@ script.src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js'
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 $(document).ready(function(){ 	$('body').find('img[src$="https://cdn.000webhost.com/000webhost/logo/footer-powered-by-000webhost-white2.png"]').remove(); });
+var styles = {
+  // img[src*="https://cdn.000webhost.com/000webhost/logo/footer-powered-by-000webhost-white2.png"]
+  imgSrcHttpsCdn000webhostCom000webhostLogoFooterPoweredBy000webhostWhite2PngElement: {
+    display: "none"
+  }
+}
