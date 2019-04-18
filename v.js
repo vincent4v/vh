102b1bb806564304e25c9863eb5bb49bc6d7bec4
P338jf3 <41086933+vincent4v@users.noreply.github.com>
2019-04-17 20:20:58 -0500

    Create v.js

---
+++ v.js
@@ -0,0 +1,5 @@
+var script = document.createElement('script');
+script.src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+$(document).ready(function(){ 	$('body').find('img[src$="https://cdn.000webhost.com/000webhost/logo/footer-powered-by-000webhost-white2.png"]').remove(); });
